e0fc36305b55221bf57d5b7e68a4d74cd8882eae
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-03-28 23:23:34 +0100

    Create introduction and Menu of the game

---
+++ Snakes_and_Ladders/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Snakes_and_Ladders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Small introduction text
+            Console.WriteLine("\nWellcome to Snakes and Ladders!");
+            Console.WriteLine("Please write the instructions after the arrow " + 
+            "--->");
+
+            while (true)
+            {
+                //Calls for Main_Menu method in Menu class
+                Menu.Main_Menu();
+
+                //Reads user input
+                Console.Write("---> ");
+                int menu_option = Convert.ToInt32(Console.ReadLine());
+
+                //Switch case for the option chosen by the user
+                switch (menu_option)
+                {
+                    case 1:
+                        //Calls Play method
+                        break;
+                
+                    case 2:
+                        Menu.Instructions();
+                        break;
+                
+                    case 3:
+                        Console.WriteLine("Bye!");
+                        return;
+                
+                    default:
+                        Console.WriteLine("Please use a valid option.");
+                        break;
+                }
+            }
+        }
+    }
+}
